664e6d44898d9136cc0ab4a99eb5fe80912e2f01
Ben Blazak <benblazak.dev@gmail.com>
2015-08-26 13:26:28 -0700

    added solution template
    so that everyone (hopefully) puts their solution in the same file, and
    to give everyone an idea of the formatting i prefer

---
+++ solution.cpp
@@ -0,0 +1,23 @@
+/* ----------------------------------------------------------------------------
+ * Copyright &copy; 2015 TODO(your name) <TODO(your email)>
+ * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+ * ------------------------------------------------------------------------- */
+
+/**
+ * TODO: program description
+ */
+
+// TODO: `#include`s and function prototypes
+
+// ----------------------------------------------------------------------------
+
+int main() {
+    // TODO
+
+    return 0;  // success
+}
+
+// ----------------------------------------------------------------------------
+
+// TODO: other functions
+
